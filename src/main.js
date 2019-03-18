06bf667c14745c6e6de542efdc7ffd5b007a7043
Chris Chan <cchan@gpeahk.org>
2019-03-18 12:27:11 +0800

    init

--- src/main.js
+++ src/main.js
@@ -3,11 +3,41 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
-
+//
+import VueYoutube from 'vue-youtube'
+Vue.use(VueYoutube)
+import Transitions from 'vue2-transitions'
+Vue.use(Transitions)
+import VueScrollTo from 'vue-scrollto';
+Vue.use(VueScrollTo, {
+    container: 'body',
+    duration: 400,
+    easing: 'easeOutQuint',
+});
+//
+import NProgress from "nprogress";
+import 'nprogress/nprogress.css'
+//
+import 'swiper/dist/css/swiper.css'
+import 'normalize.css/normalize.css'
+import 'nprogress/nprogress.css'
+import M from 'materialize-css'
+//
 Vue.config.productionTip = false
-
+    //
+    //
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    created() {
+        NProgress.start()
+    },
+    mounted() {
+        M.AutoInit()
+        this.$nextTick(() => {
+            // The whole view is rendered, so I can safely access or query the DOM
+            NProgress.done()
+        })
+    },
+    render: h => h(App)
 }).$mount('#app')
