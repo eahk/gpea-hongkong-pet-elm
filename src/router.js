56ba939c595b65b5cc4edd63270777555ff2430b
KFC <hankfchan@gmail.com>
2020-04-26 15:36:07 +0800

    update the share links

--- src/router.js
+++ src/router.js
@@ -1,28 +1,28 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from '@/views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/views/Home.vue";
 // import Legco from '@/views/Legco.vue'
-Vue.use(Router)
+Vue.use(Router);
 
 const router = new Router({
   routes: [
     {
-      path: '*',
+      path: "*",
       component: Home,
-      props: { theme: 'eco' },
+      props: { theme: "eco" }
     },
     {
-      path: '/mermaid',
-      name: 'mermaid',
+      path: "/mermaid",
+      name: "mermaid",
       component: Home,
-      props: { theme: 'mermaid' },
+      props: { theme: "eco" }
     },
     {
-      path: '/ecology',
-      name: 'ecology',
+      path: "/ecology",
+      name: "ecology",
       component: Home,
-      props: { theme: 'eco' },
-    },
+      props: { theme: "eco" }
+    }
     /*
     {
       path: '/legcotest',
@@ -30,7 +30,7 @@ const router = new Router({
       component: Legco,
     },
     */
-  ],
-})
+  ]
+});
 
-export default router
+export default router;
