4f273f76b8dad781baf6ab569bc382988cfeedb6
KFC <hankfchan@gmail.com>
2020-04-26 16:02:01 +0800

    fixing new vue cli & sass-loader errors

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,17 @@
-const isProduction = process.env.NODE_ENV === 'production'
 module.exports = {
-  publicPath: isProduction ? 'https://api.greenpeace.org.hk/2019/elm/' : '',
-  configureWebpack: config => {
-    config.output.filename = 'js/[name].js'
-    config.output.chunkFilename = 'js/[name].js'
-  },
+  css: {
+    loaderOptions: {
+      // by default the `sass` option will apply to both syntaxes
+      // because `scss` syntax is also processed by sass-loader underlyingly
+      // but when configuring the `data` option
+      // `scss` syntax requires an semicolon at the end of a statement, while `sass` syntax requires none
+      // in that case, we can target the `scss` syntax separately using the `scss` option
+      scss: {
+        prependData: `@import "~@/styles/scss/global.scss";`
+      }
+    }
+  }
+  /*
   chainWebpack: config => {
     if (config.plugins.has('extract-css')) {
       const extractCSSPlugin = config.plugin('extract-css')
@@ -21,13 +28,14 @@ module.exports = {
     svgRule.use('vue-svg-loader').loader('vue-svg-loader')
   },
   css: {
-    sourceMap: process.env.NODE_ENV !== 'production' ? true : false,
+    sourceMap: process.env.NODE_ENV !== "production" ? true : false,
     loaderOptions: {
       sass: {
         data: `
           @import "@/styles/scss/global.scss";
-        `,
-      },
-    },
-  },
-}
+        `
+      }
+    }
+  }
+  */
+};
