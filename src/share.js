4539fda8eb760c688eda91f02ed7d018a69f41fa
KFC <hankfchan@gmail.com>
2020-08-04 13:48:14 +0800

    update url

--- src/share.js
+++ src/share.js
@@ -6,7 +6,7 @@ const mainShare = event => {
   const fbShare = () => {
     var baseURL = "https://www.facebook.com/sharer/sharer.php";
     var u =
-      "https://act.greenpeace.org/page/40590/petition/1?utm_campaign=2020-elm&utm_source=facebook&utm_medium=social&utm_content=main_share#/";
+      "https://cloud.greenhk.greenpeace.org/petition-oceans-elm?utm_campaign=2020-elm&utm_source=facebook&utm_medium=social&utm_content=main_share#/";
     var t = (window.innerHeight - 436) / 2;
     var l = (window.innerWidth - 626) / 2;
     window.open(
@@ -22,7 +22,7 @@ const mainShare = event => {
         title: "",
         text:
           "海洋住屋不對立，為了珍貴脆弱的香港海洋生態，及讓市民安居樂業，一起發聲，要求政府優先發展棕地，放棄不負責任的「明日大嶼」方案！👉 ",
-        url: "https://act.gp/2VD8bGF"
+        url: "https://act.gp/2XlKLpC"
       })
       .then()
       .catch();
